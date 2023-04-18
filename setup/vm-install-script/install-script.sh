025bdd5df21b4a2ae7a22dc460d31f5bc9f0f6d2
Al West <al@tsew.net>
2023-04-18 20:34:58 +0100

    Update vm install script (#1)
    * Update install-script.sh
    
    * Update install-script.sh
    
    * Set execute bit on install-script.sh
    
    * Update to new Jenkins apt key

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo ".........----------------#################._.-.-INSTALL-.-._.#################----------------........."
 PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
@@ -15,69 +16,74 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-KUBE_VERSION=1.20.0
+KUBE_VERSION=1.26.3
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf | true
 apt-get update
-apt-get install -y kubelet=${KUBE_VERSION}-00 vim build-essential jq python3-pip docker.io kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00 kubeadm=${KUBE_VERSION}-00
+apt-get upgrade -y
+apt-get install -y kubelet=${KUBE_VERSION}-00 wget vim build-essential jq python3-pip kubectl=${KUBE_VERSION}-00 runc kubernetes-cni=1.2.0-00 kubeadm=${KUBE_VERSION}-00
+sudo apt-mark hold kubeadm kubectl kubelet
 pip3 install jc
 
 ### UUID of VM 
 ### comment below line if this Script is not executed on Cloud based VMs
 jc dmidecode | jq .[1].values.uuid -r
 
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "storage-driver": "overlay2"
-}
-EOF
-mkdir -p /etc/systemd/system/docker.service.d
-
+wget https://github.com/containerd/containerd/releases/download/v1.7.0/containerd-1.7.0-linux-amd64.tar.gz
+sudo tar Czxvf /usr/local containerd-1.7.0-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+mkdir -p /usr/lib/systemd/system
+mv containerd.service /usr/lib/systemd/system/
+mkdir -p /etc/containerd/
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
+echo "alias docker='crictl'" > /etc/profile.d/00-alises.sh
+alias docker='crictl'
 systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
+systemctl enable --now containerd
+
 systemctl enable kubelet
 systemctl start kubelet
 
 echo ".........----------------#################._.-.-KUBERNETES-.-._.#################----------------........."
-rm /root/.kube/config
+rm /root/.kube/config | true
 kubeadm reset -f
+modprobe br_netfilter
+sed -ir 's/#{1,}?net.ipv4.ip_forward ?= ?(0|1)/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+sysctl -w net.ipv4.ip_forward=1
+sed -i '/ swap / s/^ (.*)$/#1/g' /etc/fstab
+swapoff -a
 
 # uncomment below line if your host doesnt have minimum requirement of 2 CPU
 # kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
 kubeadm init --kubernetes-version=${KUBE_VERSION} --skip-token-print
 
 mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+cp -f /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
+kubectl apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
+echo "Waiting 60 seconds"
 sleep 60
 
 echo "untaint controlplane node"
-kubectl taint node $(kubectl get nodes -o=jsonpath='{.items[].metadata.name}')  node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 kubectl get node -o wide
 
-
-
 echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
-sudo apt install openjdk-8-jdk -y
+apt install openjdk-11-jdk -y
 java -version
-sudo apt install -y maven
+apt install -y maven
 mvn -v
 
-
-
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install -y jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update
+apt install -y jenkins
 systemctl daemon-reload
 systemctl enable jenkins
-sudo systemctl start jenkins
-#sudo systemctl status jenkins
-sudo usermod -a -G docker jenkins
+systemctl start jenkins
+systemctl status jenkins --no-pager
 echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo ".........----------------#################._.-.-COMPLETED-.-._.#################----------------........."
