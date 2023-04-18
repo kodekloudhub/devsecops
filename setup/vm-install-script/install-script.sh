c7bb950e3913f1f97f10c712bd842d3094eaf186
Al West <al@tsew.net>
2023-04-18 20:16:27 +0100

    Update to new Jenkins apt key

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -76,7 +76,7 @@ apt install -y maven
 mvn -v
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt update
 apt install -y jenkins
